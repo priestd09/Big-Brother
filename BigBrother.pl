6bebc977176c414cb35491c898f4ef17ef0e31c2
0x80483fb <0x80483fb@gmail.com>
2011-09-14 19:10:34 +0300

    Added logout subroutine, use warnings and $root_url

--- BigBrother.pl
+++ BigBrother.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 use strict;
+use warnings;
 
 use HTTP::Cookies;
 use WWW::Mechanize;
@@ -9,27 +10,61 @@ $mech->agent_alias('Linux Mozilla');
 $mech->cookie_jar(HTTP::Cookies->new());
 $mech->proxy('http', 'http://localhost:8118'); # Use TOR proxy
 
-my $email = 'name@email';
-my $password = 'pass';
+my $email = 'username@mail.com';
+my $password = 'password'; 
 
 # We use Facebook for mobile because it's easier to deal with
-my $index_url = 'http://m.facebook.com/index.php';
-my $profile_url = 'http://m.facebook.com/profile.php';
-my $connect_url = 'http://m.facebook.com/connect.php?id='; # followed with id number
-my $findfr_url = 'http://m.facebook.com/findfriends.php'; # which we got from this url
+my $root_url = 'http://m.facebook.com/';
+my $index_url = $root_url . 'index.php';
+my $profile_url = $root_url . 'profile.php';
+my $connect_url = $root_url . 'connect.php?id='; # followed with id number
+my $findfr_url = $root_url . 'findfriends.php'; # which we got from this url
+my $get_error = "Error fetching content of ";
 
 login($email, $password);
-my @newfriends = find_friends();
-# ^ not sure if works, got blocked from fb before testing :(
-# should have used proxies.
+print "Logged in\n";
+
+logout();
+print "Logged out\n";
+
+sub login {
+	$mech->get($index_url) or die "$get_error.$index_url\n";
+
+	my $response = $mech->submit_form(
+	  fields => {
+	    email => $_[0],
+	    pass => $_[1]
+	  }
+	);
+
+	die $response->status_line unless $response->status_line;
+	# should add a piece of code here that tells us if we got blocked
+	# otherwise it would just stop working and we would have no idea why
+}
+
+sub logout {
+	$mech->get($index_url) or die "$get_error.$index_url\n";
+	my $content = $mech->content();
+
+	my $logout;
+	if ($content =~ /(logout\.php(.+?)")/) {
+		$logout = $1;
+		$logout =~ s/"$//g;
+	}
+	
+	$mech->get($root_url . $logout) or die "Error logging out\n";
+}
+
+# Not yet in use subs
 
 sub find_friends {
-	$mech->get($findfr_url);
-	my $content = $mech->content() or die "Error getting findfriends.php content\n";
+	$mech->get($findfr_url) or die "Error fetching $findfr_url content\n";
+	my $content = $mech->content() or die "$get_error.$findfr_url\n";
 
 	while ($content =~ /connect\.php\?id=(\d+)&/g) {
 		print $1. "\n";
 		push(@_, $1);
+		print "Lol\n";
 	}
 
 	return @_;
@@ -42,18 +77,4 @@ sub post_status {
 		fields => { status => shift }
 	); 
 	die $response->status_line unless $response->status_line;
-}
-
-sub login {
-	$mech->get($index_url);
-
-	my $response = $mech->submit_form(
-	  fields => {
-	    email => @_[0],
-	    pass => @_[1]
-	  }
-	);
-	die $response->status_line unless $response->status_line;
-	# should add a piece of code here that tells us if we got blocked
-	# otherwise it would just stop working and we would have no idea why
 }
