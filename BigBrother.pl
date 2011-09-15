ab6159e4b3225bf4441592beb6829d4a0620c561
Tom <0x80483fb@gmail.com>
2011-09-15 16:13:27 +0200

    Added get() subroutine, so that $mech->get() keeps trying

--- BigBrother.pl
+++ BigBrother.pl
@@ -11,7 +11,7 @@ $mech->cookie_jar(HTTP::Cookies->new());
 $mech->proxy('http', 'http://localhost:8118'); # Use TOR proxy
 
 my $email = 'username@mail.com';
-my $password = 'password'; 
+my $password = 'password';
 
 # We use Facebook for mobile because it's easier to deal with
 my $root_url = 'http://m.facebook.com/';
@@ -19,16 +19,18 @@ my $index_url = $root_url . 'index.php';
 my $profile_url = $root_url . 'profile.php';
 my $connect_url = $root_url . 'connect.php?id='; # followed with id number
 my $findfr_url = $root_url . 'findfriends.php'; # which we got from this url
-my $get_error = "Error fetching content of ";
 
 login($email, $password);
-print "Logged in\n";
-
 logout();
-print "Logged out\n";
+
+sub get {
+	my $url = $_[0];
+	$mech->get($url);
+	get() unless ($mech->success());
+}
 
 sub login {
-	$mech->get($index_url) or die "$get_error.$index_url\n";
+	get($index_url);
 
 	my $response = $mech->submit_form(
 	  fields => {
@@ -36,14 +38,10 @@ sub login {
 	    pass => $_[1]
 	  }
 	);
-
-	die $response->status_line unless $response->status_line;
-	# should add a piece of code here that tells us if we got blocked
-	# otherwise it would just stop working and we would have no idea why
 }
 
 sub logout {
-	$mech->get($index_url) or die "$get_error.$index_url\n";
+	get($index_url);
 	my $content = $mech->content();
 
 	my $logout;
@@ -51,14 +49,14 @@ sub logout {
 		$logout = $1;
 	}
 	
-	$mech->get($root_url . $logout) or die "Error logging out\n";
+	get($root_url . $logout);
 }
 
 # Not yet in use subs
 
 sub find_friends {
-	$mech->get($findfr_url) or die "Error fetching $findfr_url content\n";
-	my $content = $mech->content() or die "$get_error.$findfr_url\n";
+	get($findfr_url);
+	my $content = $mech->content();
 
 	while ($content =~ /connect\.php\?id=(\d+)&/g) {
 		print $1. "\n";
@@ -71,7 +69,7 @@ sub find_friends {
 
 # no use for this yet but maybe in the future? :)
 sub post_status {
-	$mech->get($profile_url);
+	get($profile_url);
 	my $response = $mech->submit_form(
 		fields => { status => shift }
 	); 
