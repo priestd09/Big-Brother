30fe87724e428cc8656ca12e1e484dd9aa320044
jolttz <jolttz@gmail.com>
2011-09-15 21:39:17 +0300

    small fixes and not working find_friends

--- BigBrother.pl
+++ BigBrother.pl
@@ -10,32 +10,35 @@ $mech->agent_alias('Linux Mozilla');
 $mech->cookie_jar(HTTP::Cookies->new());
 $mech->proxy('http', 'http://localhost:8118'); # Use TOR proxy
 
-my $email = 'username@mail.com';
-my $password = 'password';
+my $email = 'coudraep@deagot.com';
+my $password = 'haidermary';
 
 # We use Facebook for mobile because it's easier to deal with
 my $root_url = 'http://m.facebook.com/';
 my $index_url = $root_url . 'index.php';
 my $profile_url = $root_url . 'profile.php';
 my $connect_url = $root_url . 'connect.php?id='; # followed with id number
-my $findfr_url = $root_url . 'findfriends.php'; # which we got from this url
+my $findfr_url = $root_url . 'findnetfriends.php'; # which we got from this url
 my $tries = 0;
 
 login($email, $password);
+find_friends("California", 2005);
 logout();
 
-sub get {
+sub get_content {
 	my $url = $_[0];
 	$mech->get($url);
 	$tries ++;
-	if ($tries < 3) {
-		get() unless ($mech->success());
+	if (($tries < 3) and (!$mech->success())) {
+		print "Failed to get content for: $url\n";
+		get_content($url)
 	}
 	$tries = 0;
+	return $mech->content();
 }
 
 sub login {
-	get($index_url);
+	my $content = get_content($index_url);
 
 	my $response = $mech->submit_form(
 	  fields => {
@@ -46,35 +49,34 @@ sub login {
 }
 
 sub logout {
-	get($index_url);
-	my $content = $mech->content();
+	my $content = get_content($index_url);
 
 	my $logout;
 	if ($content =~ /(logout\.php.+?)"/) {
 		$logout = $1;
 	}
 	
-	get($root_url . $logout);
+	get_content($root_url . $logout);
 }
 
 # Not yet in use subs
 
 sub find_friends {
-	get($findfr_url);
-	my $content = $mech->content();
+	my $content = get_content($findfr_url);
 
-	while ($content =~ /connect\.php\?id=(\d+)&/g) {
-		print $1. "\n";
-		push(@_, $1);
-		print "Lol\n";
-	}
+	my $response = $mech->submit_form(
+	  fields => {
+	    sf_text_field => $_[0], # search field
+		sf_year_field => $_[1] # year field
+	  }
+	);
 
-	return @_;
+	print $response->decoded_content;
 }
 
 # no use for this yet but maybe in the future? :)
 sub post_status {
-	get($profile_url);
+	my $content = get_content($profile_url);
 	my $response = $mech->submit_form(
 		fields => { status => shift }
 	); 
